c95cafacf9cdf621362456bd153075f08df448fd
lvxiaobu <lhg_523@163.com>
2019-09-18 20:50:09 +0800

    完成表单数据的(单个)自动校验和(整个)手动校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -7,7 +7,8 @@
         </div>
         <!-- 表单====>  el-form包裹 -->
         <!-- 数据校验 => el-form 1、绑定表单数据 model 2、绑定数据校验规则 rules-->
-        <el-form :model='loginForm' :rules='loginRules' style="margin-top='20px'" ref="">
+        <!-- ref="myForm"：把整个表单 el-form 通过 ref 的形式传入 Vue.$refs 中 -->
+        <el-form :model='loginForm' :rules='loginRules' status-icon style="margin-top='20px'" ref="myForm">
            <!-- form-item 下面有一个prop属性 绑定下面表单组件 loginForm 下的对应的属性，才可以完成校验  -->
            <el-form-item prop='mobile'>
               <!-- 手机号 绑定 v-model-->
@@ -28,7 +29,9 @@
 
            <el-form-item>
               <!-- 登录按钮 -->
-              <el-button type='primary' style="width:100%">登录</el-button>
+              <!-- 添加点击事件 在点击的时候，校验转整个表单的数据 -->
+              <!-- type='primary':给按钮一个颜色（elementUI中自带） -->
+              <el-button @click="login" type='primary' style="width:100%">登录</el-button>
            </el-form-item>
         </el-form>
      </el-card>
@@ -38,6 +41,17 @@
 <script>
 export default {
   data () {
+    // 自定义校验函数 validator  在 loginRules 中的 agree 使用
+    let validator = function (rule, value, callback) {
+      // =====第一种写法=====
+      // if (value) { // 正确 勾选协议
+      //   callback() // 一切OK继续执行
+      // } else { // 不正确 不勾选协议
+      //   callback(new Error('你必须选'))
+      // }
+      // =====第二种写法=====
+      value ? callback() : callback(new Error('你必须选'))
+    }
     return {
       loginForm: { // 表单数据
         mobile: '', // 手机号
@@ -45,12 +59,27 @@ export default {
         agree: false // 是否同意协议
       },
       loginRules: { // 表单规则对象集合
-
+        // key(表单各个组件的字段名):[{},{},{}]
+      // form-item中参数  required 为true 表示该字段必填 如果不填，就会提示信息
+        mobile: [{ required: true, message: '请输入你的手机号' }, { pattern: /^1[3456789]\d{9}$/, message: '请输入合法的手机号' }],
+        code: [{ required: true, message: '请输入验证码' }, { pattern: /^\d{6}$/, message: '请输入6位数的验证码' }],
+        // 因为 agree 判断的是布尔值，
+        // 如果数据校验不满足，还可以自定义校验函数 validator
+        // validator：一个自定义函数 validator:funtion (rule(当前规则),value(当前值),callback(回调函数){})
+        agree: [{ validator }]
       }
     }
   },
   methods: {
-
+    // 添加点击事件 校验整个表单
+    login () {
+      //  this.$refs:获取（对象实例）DOM对象
+      // 只有一切的校验通过之后 才会进行请求
+      // validate 是一个方法 => 方法中传入的一个函数 两个校验参数  是否校验成功(有用)/未校验成功的字段(鸡肋)
+      this.$refs.myForm.validate(function (isOK) {
+        isOK ? console.log('校验成功') : console.log('校验失败')
+      })
+    }
   },
   components: {
 
