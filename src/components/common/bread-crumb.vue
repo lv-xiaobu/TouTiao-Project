865c36936e1dea5cf44bb3b3f40a065e62e1a869
lvxiaobu <lhg_523@163.com>
2019-09-22 21:54:32 +0800

    创建面包屑

---
+++ src/components/common/bread-crumb.vue
@@ -0,0 +1,29 @@
+<template>
+<!-- separator：分隔符 -->
+  <el-breadcrumb separator=">">
+  <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+  <el-breadcrumb-item><a href="/">
+  <slot name="title"></slot>
+  </a></el-breadcrumb-item>
+</el-breadcrumb>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 // 引入自定义组件，让他们可以全局使用
 import layoutAside from './home/layout-aside'
 import layoutHeader from './home/layout-header'
+import breadCrumb from './common/bread-crumb'
 
 // 实现整体组件的统一注册
 // 在main.ls的文件中，Vue实例 之前引入
@@ -10,5 +11,6 @@ export default {
     // 可以在实例化之前注册组件 参考全局组件
     Vue.component('layout-aside', layoutAside) // 全局注册左侧导航
     Vue.component('layout-header', layoutHeader) // 全局注册头部导航
+    Vue.component('bread-crumb', breadCrumb) // 全局注册面包屑
   }
 }
