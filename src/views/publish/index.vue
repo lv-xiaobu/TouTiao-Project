35b62c5ee556c1509f7506eede06afaf4e3d4c54
lvxiaobu <lhg_523@163.com>
2019-09-25 15:12:00 +0800

    发表文章页面-发表文章功能

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -27,8 +27,8 @@
                </el-select>
            </el-form-item>
            <el-form-item>
-               <el-button type='primary' @click='publish'>发布文章</el-button>
-               <el-button>存入草稿</el-button>
+               <el-button type='primary' @click='publish(false)'>发布文章</el-button>
+               <el-button @click='publish(true)'>存入草稿</el-button>
            </el-form-item>
 
       </el-form>
@@ -51,7 +51,10 @@ export default {
       },
       // ===== 校验规则 =====
       rules: {
-        title: [{ required: true, message: '标题不为空' }],
+        title: [{ required: true, message: '标题不为空' }, {
+          // 最小长度 min  最大长度 max
+          min: 5, max: 30, message: '标题长度要控制在5到30个字符之间'
+        }],
         content: [{ required: true, message: '内容不为空' }],
         channel_id: [{ required: true, message: '频道不为空' }]
       }
@@ -67,11 +70,19 @@ export default {
       })
     },
     // ===== 发布文章 =====
-    publish () {
+    publish (draft) {
       debugger
       this.$refs.publishForm.validate((isOk) => {
         if (isOk) {
-
+          this.$axios({
+            url: '/articles',
+            method: 'post',
+            params: { draft: draft }, // 是否存为草稿（true 为草稿,false 为正常）
+            data: this.formData
+          }).then(() => {
+            // 发布成功 => 回到内容列表
+            this.$router.push('/home/articles')
+          })
         }
       })
     }
