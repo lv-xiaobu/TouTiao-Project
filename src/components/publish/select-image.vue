793b75da99585414178e5b0b1b9c1caa317ff2e5
lvxiaobu <lhg_523@163.com>
2019-09-27 00:33:29 +0800

    发布文章-封面图片选择组件上传

--- src/components/publish/select-image.vue
+++ src/components/publish/select-image.vue
@@ -21,7 +21,12 @@
             </el-pagination>
          </el-row>
      </el-tab-pane>
-     <el-tab-pane label='上传图片'></el-tab-pane>
+     <el-tab-pane label='上传图片'>
+        <!-- 自定义上传 -->
+         <el-upload :show-file-list="false" action="" :http-request="uploadImg">
+            <i class="el-icon-plus avatar-uploader-icon"></i>
+         </el-upload>
+     </el-tab-pane>
   </el-tabs>
 </template>
 
@@ -38,6 +43,19 @@ export default {
     }
   },
   methods: {
+    // ===== 上传用户素材 =====
+    uploadImg (params) {
+      let data = new FormData()
+      data.append('image', params.file)
+      this.$axios({
+        url: '/user/images',
+        method: 'post',
+        data
+      }).then(result => {
+        //   得到一个线上地址
+        this.$emit('selectOneImg', result.data.url)
+      })
+    },
     // ===== 点击素材图片，获取图片的id =====
     clickImg (item) {
       // 利用自定义事件，把 url 传递给父组件 cover-images 在组件实例中接收
@@ -67,19 +85,42 @@ export default {
 </script>
 
 <style lang='less' scoped>
-   .imgs-list {
-      display: flex;
-      flex-wrap: wrap;
-      justify-content: center;
-      .imgs-item {
-         width: 120px;
-         height: 120px;
-         margin:10px;
-         line-height: 120px;
-         img {
-            width: 100%;
-            height: 100%;
-         }
+.imgs-list {
+   display: flex;
+   flex-wrap: wrap;
+   justify-content: center;
+   .imgs-item {
+      width: 120px;
+      height: 120px;
+      margin:10px;
+      line-height: 120px;
+      img {
+         width: 100%;
+         height: 100%;
       }
    }
+}
+.avatar-uploader .el-upload {
+   border: 1px dashed #d9d9d9;
+   border-radius: 6px;
+   cursor: pointer;
+   position: relative;
+   overflow: hidden;
+}
+.avatar-uploader .el-upload:hover {
+   border-color: #409EFF;
+}
+.avatar-uploader-icon {
+   font-size: 28px;
+   color: #8c939d;
+   width: 178px;
+   height: 178px;
+   line-height: 178px;
+   text-align: center;
+}
+.avatar {
+   width: 178px;
+   height: 178px;
+   display: block;
+}
 </style>
