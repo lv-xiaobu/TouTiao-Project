5eeb500e1346b32c52da9b4033defee885b563b7
lvxiaobu <lhg_523@163.com>
2019-09-27 20:51:42 +0800

    账户信息-用户信息保存

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -6,21 +6,21 @@
       <template slot="title">账户信息</template>
     </bread-crumb>
     <!-- 表单 -->
-    <el-form style="margin-left:60px" label-width='100px'>
-       <el-form-item label='用户名'>
+    <el-form ref="accountformDate" :rules='accountRules' :model="formData" style="margin-left:60px" label-width='100px'>
+       <el-form-item prop='name' label='用户名'>
           <el-input v-model="formData.name" style='width:300px'></el-input>
        </el-form-item>
        <el-form-item label='简介'>
           <el-input v-model="formData.intro" style='width:300px'></el-input>
        </el-form-item>
-       <el-form-item label='邮箱'>
+       <el-form-item prop='email' label='邮箱'>
           <el-input v-model="formData.email" style='width:300px'></el-input>
        </el-form-item>
        <el-form-item label='手机号'>
           <el-input placeholder='我是你爸爸' disabled v-model="formData.mobile" style='width:300px'></el-input>
        </el-form-item>
        <el-form-item lable=''>
-          <el-button type='primary'>保存信息</el-button>
+          <el-button @click="saveUser" type='primary'>保存信息</el-button>
        </el-form-item>
     </el-form>
     <img class="head-img" :src="formData.photo ? formData.photo : defaultImg" alt="" srcset="">
@@ -32,10 +32,29 @@ export default {
   data () {
     return {
       formData: {},
-      defaultImg: require('../../assets/img/cai.gif')
+      defaultImg: require('../../assets/img/cai.gif'),
+      accountRules: {
+        name: [{ required: true, message: '用户名不能为空' }, { min: 1, max: 7, message: '1~7个字符' }],
+        email: [{ required: true, message: '邮箱不能为空' }, { pattern: /\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/, message: '邮箱格式不正确' }]
+      } // 校验规则
     }
   },
   methods: {
+    // ===== 保存账户信息 =====
+    saveUser () {
+      this.$refs.accountformDate.validate((isOK) => {
+        if (isOK) {
+          this.$axios({
+            url: '/user/profile',
+            method: 'patch',
+            data: this.formData
+          }).then(() => {
+            //   成功提示消息
+            this.$message({ message: '保存成功', type: 'success' })
+          })
+        }
+      })
+    },
     // ===== 获取账户信息 =====
     getUserInfo () {
       this.$axios({
@@ -46,7 +65,6 @@ export default {
     }
   },
   created () {
-    debugger
     this.getUserInfo()
   }
 }
