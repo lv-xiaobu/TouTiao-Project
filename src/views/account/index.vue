4491b22704a873247a297aaa4e18675579af51b1
lvxiaobu <lhg_523@163.com>
2019-09-27 20:03:50 +0800

    账户信息-页面渲染数据

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -8,32 +8,47 @@
     <!-- 表单 -->
     <el-form style="margin-left:60px" label-width='100px'>
        <el-form-item label='用户名'>
-          <el-input style='width:300px'></el-input>
+          <el-input v-model="formData.name" style='width:300px'></el-input>
        </el-form-item>
        <el-form-item label='简介'>
-          <el-input style='width:300px'></el-input>
+          <el-input v-model="formData.intro" style='width:300px'></el-input>
        </el-form-item>
        <el-form-item label='邮箱'>
-          <el-input style='width:300px'></el-input>
+          <el-input v-model="formData.email" style='width:300px'></el-input>
        </el-form-item>
        <el-form-item label='手机号'>
-          <el-input style='width:300px'></el-input>
+          <el-input placeholder='我是你爸爸' disabled v-model="formData.mobile" style='width:300px'></el-input>
        </el-form-item>
        <el-form-item lable=''>
           <el-button type='primary'>保存信息</el-button>
        </el-form-item>
     </el-form>
-    <img class="head-img" src="../../assets/img/cai.gif" alt="" srcset="">
+    <img class="head-img" :src="formData.photo ? formData.photo : defaultImg" alt="" srcset="">
   </el-card>
 </template>
 
 <script>
 export default {
   data () {
-    return {}
+    return {
+      formData: {},
+      defaultImg: require('../../assets/img/cai.gif')
+    }
   },
-  methods: {},
-  components: {}
+  methods: {
+    // ===== 获取账户信息 =====
+    getUserInfo () {
+      this.$axios({
+        url: '/user/profile'
+      }).then(result => {
+        this.formData = result.data
+      })
+    }
+  },
+  created () {
+    debugger
+    this.getUserInfo()
+  }
 }
 </script>
 
